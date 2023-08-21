ffaf505de8196a16b362d9b8e5b7e9457c8ab1e9
Sultan <ffkirito6@gmail.com>
2023-08-22 00:27:34 +0600

    new effect

--- src/components/Cart-icon.jsx
+++ src/components/Cart-icon.jsx
@@ -25,32 +25,3 @@ const CartIcon = () => {
 };
 
 export default CartIcon;
-
-
-
-
-// import React from 'react';
-// import { useSelector } from 'react-redux';
-
-// const CartIcon = () => {
-//   const count_products = useSelector((state) => state.bag.length);
-//   const open = () => {
-//     document.querySelector('.bag').style.display = 'flex';
-//     document.querySelector('body').style.overflowY = 'hidden';
-//   };
-//   const hideCounter = document.querySelector('.countIcon');
-
-//   if (count_products) {
-//     hideCounter.style.display = 'block';
-//   }
-
-//   return (
-//     <div className="cart-icon" onClick={() => open()}>
-//       <span className="countIcon">
-//         {count_products === 0 ? '' : count_products}
-//       </span>
-//     </div>
-//   );
-// };
-
-// export default CartIcon;
